d44b1de29de43a08f46af2f181e0c2b443e0770b
fade0529 <merlincc2002@gmail.com>
2021-05-05 18:28:41 +0800

    test

---
+++ random.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main()
+{
+	
+	FILE *text1, *text2;
+	srand(time(NULL));
+	char chars[] = "abcdefghijklmnopqrstuvwxyz";
+        int nchars = strlen(chars);
+	text1=fopen("dataset1.txt", "w");
+	text2=fopen("dataset2.txt", "w");
+	if((text1==NULL)||(text2==NULL))
+	{
+		printf("Error");
+		return (-1);
+	}
+	for(int i=0;i<1000000;i++)
+	{
+		fprintf(text1, "%d\n", (int)rand());
+        	for (int j = 0; j < 100; j++)
+        		fputc(chars[(int) ((rand() / ((double) RAND_MAX + 1)) * nchars)], text2);
+		fputs("\n", text2);
+		
+	}	
+	fclose(text1);
+	fclose(text2);
+	return 0;
+}
