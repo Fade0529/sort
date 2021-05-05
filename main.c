c5b6f0397975aa1d9a5c69d95badfe046881c3e8
Fade0529 <79902412+Fade0529@users.noreply.github.com>
2021-05-05 19:33:51 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
-#include f1.h
+#include "random.h"
+#include "f1.h"
+#include "f2.h"
+#inculde "f3.h"
 #include <stdio.h>
 
 int main()
-{
+{       print_random();
+        pirnt_f1();
+        print_f2();
+        print_f3();
 	return 0;	
 }
